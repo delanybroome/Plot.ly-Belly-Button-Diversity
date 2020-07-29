1e85795f8111c18a8c2b5b516095ae60e887491c
delanybroome <delany.broome@waterboards.ca.gov>
2020-07-28 22:42:49 -0700

    first lines of code

---
+++ plotly.js
@@ -0,0 +1,10 @@
+//Step 1: Use D3 Library to Read JSON data file
+//Create Horizontal Bar Chart With  drop Down Menu to display top 10 OTUS fuond in that indv.
+function buildPlot(BellySamples) {
+    d3.json("samples.json").then(function(bellydata) {
+        console.log(bellydata)
+        var samples = data.samples;
+
+    }
+
+}
