3737084cf481a7b0bb417786c28e34b16bf3f414
delanybroome <delany.broome@waterboards.ca.gov>
2020-07-28 23:29:03 -0700

    bar chart

--- plotly.js
+++ plotly.js
@@ -9,7 +9,34 @@ function buildChart(BellySamples) {
         var otu_ids = bellydata.otu_ids;
     //Use `otu_labels` as the hovertext for the chart.
         var otu_labels = bellydata.otu_labels
+    //Slice the first 10 object for plotting and Reverse Array
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var trace1 = {
+          type: "bar",
+          name: "Bar Chart"
+          y: yticks,
+          orientation: "h" 
+        };
+        var barData = [trace1];
+          Plotly.newPlot("bar", barData);
+        });
+//Create a Bubble Chart
+// Use `otu_ids` for the x values.
 
+// Use `sample_values` for the y values.
+
+// Use `sample_values` for the marker size.
+
+// Use `otu_ids` for the marker colors.
+
+// Use `otu_labels` for the text values.
+
+//Display Sample Meta Data 
+
+//Display each key-value pair from the
+// metadata JSON object somewhere on the page.
+
+//Update all of the plots any time that a new sample is selected.
     }
 
 }
