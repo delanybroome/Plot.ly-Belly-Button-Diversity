9a594345bf675fabd42c9d20f9c3033a886fe9ee
delanybroome <delany.broome@waterboards.ca.gov>
2020-09-19 13:24:29 -0700

    update

--- app.js
+++ app.js
@@ -1,48 +1,3 @@
-//Step 1: Use D3 Library to Read JSON data file
-function buildChart(BellySamples) {
-    d3.json("samples.json").then((bellydata) => {
-        //Create Horizontal Bar Chart With  drop Down Menu to display top 10 OTUS fuond in that indv.
-        console.log(bellydata)
-    //Sample_Values for values 
-        var Sample_Values = bellydata.Sample_Values;
-        console.log(Sample_Values)
-    //Use `otu_ids` as the labels for the bar chart.
-        var otu_ids = bellydata.otu_ids;
-        console.log(otu_ids)
-    //Use `otu_labels` as the hovertext for the chart.
-        var otu_labels = bellydata.otu_labels
-        console.log(otu_labels)
-    //Slice the first 10 object for plotting and Reverse Array
-        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
-        var trace1 = {
-          type: "bar",
-          name: "Bar Chart"
-          y: yticks,
-          orientation: "h" 
-        };
-        var barData = [trace1];
-          Plotly.newPlot("bar", barData);
-        };
-//Create a Bubble Chart
-// Use `otu_ids` for the x values.
-var trace2 = {
-    x: samples.otuID
-// Use `sample_values` for  the y values.
-    y: samples.sample_values, 
-    mode: "markers", 
-    marker: {
-// Use `sample_values` for the marker size.
-        size: samples.sample_values, 
-// Use `otu_ids` for the marker colors.
-        color: samples.otu_ids
-    },
-// Use `otu_labels` for the text values.
-    text: samples.otu_labels
-};
-
-var bubble_data = [trace2];
-Plotly.newPlot("bubble", bubble_data);
-
 //Display Sample Meta Data 
 function getInfo(id) {
     d3.json("samples.json").then((bellydata) => {
@@ -60,6 +15,52 @@ function getInfo(id) {
 
     });
 }
+
+//Step 1: Use D3 Library to Read JSON data file
+function buildChart(BellySamples) {
+    d3.json("samples.json").then((bellydata) => {
+        //Create Horizontal Bar Chart With  drop Down Menu to display top 10 OTUS fuond in that indv.
+        //console.log(bellydata)
+    //Sample_Values for values 
+        var Sample_Values = bellydata.Sample_Values;
+        //console.log(Sample_Values)
+    //Use `otu_ids` as the labels for the bar chart.
+        var otu_ids = bellydata.otu_ids;
+        //console.log(otu_ids)
+    //Use `otu_labels` as the hovertext for the chart.
+        var otu_labels = bellydata.otu_labels
+        //console.log(otu_labels)
+    //Slice the first 10 object for plotting and Reverse Array
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var trace1 = {
+          type: "bar",
+          name: "Bar Chart",
+          y: yticks,
+          orientation: "h" 
+        };
+        var barData = [trace1];
+          Plotly.newPlot("bar", barData);
+        
+//Create a Bubble Chart
+// Use `otu_ids` for the x values.
+var trace2 = {
+    x: otuID,
+// Use `sample_values` for  the y values.
+    y: sample_values, 
+    test: out_labels,
+    mode: "markers", 
+    marker: {
+// Use `sample_values` for the marker size.
+        size: sample_values, 
+// Use `otu_ids` for the marker colors.
+        color: otu_ids
+    },
+// Use `otu_labels` for the text values.
+    text: otu_labels
+};
+
+var bubble_data = [trace2];
+Plotly.newPlot("bubble", bubble_data);
 
 //Update all of the plots any time that a new sample is selected.
 function optionChanged(id) {
